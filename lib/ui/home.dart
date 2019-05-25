016e35441012fa769f74a2bac95fc705da62b1f0
dekdew <passawit1999@gmail.com>
2019-05-25 12:29:36 +0700

    home

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,73 +1,171 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class Home extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../utils/currentUser.dart';
+
+SharedPreferences sharedPreferences;
+
+class Home extends StatefulWidget {
   @override
+  State<StatefulWidget> createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  String _data = '';
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    // For your reference print the AppDoc directory
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/data.txt');
+  }
+
+  Future<File> writeContent(String data) async {
+    final file = await _localFile;
+    file.writeAsString('$data');
+    return file;
+  }
+
+  Future<String> readContent() async {
+    try {
+      final file = await _localFile;
+      // Read the file
+      String contents = await file.readAsString();
+      print(contents);
+      this._data = contents;
+      CurrentUser.quote = contents;
+      return this._data;
+    } catch (e) {
+      // If there is an error reading, return a default String
+      return '';
+    }
+  }
+
+  @override
+  void setState(fn) {
+    super.setState(fn);
+    readContent();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 5,
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            "Home",
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        bottomSheet: Container(
-          color: Colors.lightBlue,
-          child: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.home),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: Container(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text('Hello ' + CurrentUser.name),
+              subtitle: Text('This is my quote "' + CurrentUser.quote + '"'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                child: Text('PROFILE SETUP'),
+                onPressed: () {
+                  
+                },
               ),
-              Tab(
-                icon: Icon(Icons.notifications),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                child: Text('MY FRIENDS'),
+                onPressed: () {
+                  
+                },
               ),
-              Tab(
-                icon: Icon(Icons.explore),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                child: Text('SIGN OUT'),
+                onPressed: () {
+                  clearSharedPreferences() async {
+                  sharedPreferences = await SharedPreferences.getInstance();
+                  sharedPreferences.setString('userId', '');
+                  sharedPreferences.setString('name', '');
+                }
+
+                clearSharedPreferences();
+                writeContent('');
+
+                CurrentUser.userId = null;
+                CurrentUser.name = null;
+                CurrentUser.age = null;
+                CurrentUser.password = null;
+                CurrentUser.quote = null;
+                Navigator.of(context).pushReplacementNamed('/');
+                },
               ),
-              Tab(
-                icon: Icon(Icons.person),
-              ),
-              Tab(
-                icon: Icon(Icons.settings),
-              ),
-            ],
-          ),
-        ),
-        body: Container(
-          child: TabBarView(
-            children: <Widget>[
-              Center(
-                  child: Text(
-                "Home",
-                style: TextStyle(fontSize: 30),
-              )),
-              Center(
-                  child: Text(
-                "Notify",
-                style: TextStyle(fontSize: 30),
-              )),
-              Center(
-                  child: Text(
-                "Map",
-                style: TextStyle(fontSize: 30),
-              )),
-              Center(
-                  child: Text(
-                "Profile",
-                style: TextStyle(fontSize: 30),
-              )),
-              Center(
-                  child: Text(
-                "Setup",
-                style: TextStyle(fontSize: 30),
-              )),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: Text("Home"),
+  //     ),
+  //     body: Container(
+  //       child: ListView(
+  //         padding: const EdgeInsets.fromLTRB(20, 15, 20, 0),
+  //         children: <Widget>[
+  //           ListTile(
+  //             title: Text('Hello ${CurrentUser.name}'),
+  //             subtitle: Text(
+  //                 'this is my quote "${ CurrentUser.quote == null ? '' : CurrentUser.quote}"'),
+  //           ),
+  //           RaisedButton(
+  //             child: Text("PROFILE SETUP"),
+  //             onPressed: () {
+  //               Navigator.of(context).pushNamed('/profile');
+  //             },
+  //           ),
+  //           RaisedButton(
+  //             child: Text("MY FRIENDS"),
+  //             onPressed: () {
+  //               Navigator.of(context).pushReplacementNamed('/friend');
+  //             },
+  //           ),
+  //           RaisedButton(
+  //             child: Text("SIGN OUT"),
+  //             onPressed: () {
+  //               clearSharedPreferences() async {
+  //                 sharedPreferences = await SharedPreferences.getInstance();
+  //                 sharedPreferences.setString('userId', '');
+  //                 sharedPreferences.setString('name', '');
+  //               }
+
+  //               clearSharedPreferences();
+  //               writeContent('');
+
+  //               CurrentUser.userId = null;
+  //               CurrentUser.name = null;
+  //               CurrentUser.age = null;
+  //               CurrentUser.password = null;
+  //               CurrentUser.quote = null;
+  //               Navigator.of(context).pushReplacementNamed('/');
+  //             },
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
+
 }
